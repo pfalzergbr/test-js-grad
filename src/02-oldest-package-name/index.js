8a89c71bc65999d9d85f5ebc269a6bced5910ac5
Gabor Pfalzer <pfalzer@gmail.com>
2021-05-18 09:39:34 +0100

    solve second challenge

--- src/02-oldest-package-name/index.js
+++ src/02-oldest-package-name/index.js
@@ -27,9 +27,31 @@ The results should have this structure:
  *  With the results from this request, inside "content", return
  *  the "name" of the package that has the oldest "date" value
  */
+const axios = require('axios');
 
 module.exports = async function oldestPackageName() {
-  // TODO
+  const response = await axios.post(
+    'http://ambush-api.inyourarea.co.uk/ambush/intercept',
+    {
+      url: 'https://api.npms.io/v2/search/suggestions?q=react',
+      method: 'GET',
+      return_payload: true,
+    },
+  );
+  const { content } = response.data;
+  /* Set up a name and an oldest date variable to set in the for loop.
+  Name will be set together with any date that is older than the previous ones. */
+  let name = '';
+  let oldestDate = Date.now();
 
-  return name
+  for (i in content) {
+    // Convert the date from the fetched data to a Date, consistent with the oldest date starting point
+    const packageDate = new Date(content[i].package.date).getTime();
+    const packageName = content[i].package.name;
+    if (packageDate < oldestDate) {
+      oldestDate = packageDate;
+      name = packageName;
+    }
+  }
+  return name;
 };
